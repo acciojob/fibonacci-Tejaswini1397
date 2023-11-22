d6813e5c2fc3491d799d872195bca71243ca83ee
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 15:49:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	 const fibonacciArray = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        const nextFibonacci = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        fibonacciArray.push(nextFibonacci);
+    }
+
+    return fibonacciArray;
+	
 }
 
 module.exports = fibonacci;
