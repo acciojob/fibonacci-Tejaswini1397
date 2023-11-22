6a9211c8212002aa2bd5ed71fbb2d68ca91d236c
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 15:53:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function fibonacci(num) {
-// your code here
-	 const fibonacciArray = [0, 1];
-
-    for (let i = 2; i <= num; i++) {
-        const nextFibonacci = fibonacciArray[i - 1] + fibonacciArray[i - 2];
-        fibonacciArray.push(nextFibonacci);
+   if (num < 0 || num > 50) {
+        return "Invalid input. Please provide a positive integer within the range [0, 50].";
     }
 
-    return fibonacciArray;
-	
+    let a = 0, b = 1, temp;
+
+    for (let i = 2; i <= num; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return a;
 }
 
 module.exports = fibonacci;
